b8a5a58470cfbf1bb6cefcddbcc4290810d54e22
Cookinou <loic.coissard@gmail.com>
2021-08-30 06:51:18 +0200

    correction: add Date and Author on posts view

--- Views/posts.view.php
+++ Views/posts.view.php
@@ -6,6 +6,7 @@
 		<div class="card mb-4">
 			<div class="card-body">
 				<h5 class="card-title"><?= htmlentities($post->getTitle()) ?></h5>
+				<p class="text-muted">Publié le <?= $post->getCreatedAt()->format('d/m/Y') ?> par <?= htmlentities($post->getAuthor()) ?></p>
 				<p><?= nl2br(htmlentities($post->getDescription())) ?></p>
 				<p><a href="<?= $router->generate("show-post", ["id" => $post->getId()]) ?>" class="btn btn-primary">Voir l'article</a></p>
 			</div>
