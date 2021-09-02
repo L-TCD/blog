aa647385445e21818e381922507c071a79eeeb22
Cookinou <loic.coissard@gmail.com>
2021-09-02 17:17:06 +0200

    test codacy/fix Usercontroller $_POST problems

--- Utils/Alert.php
+++ Utils/Alert.php
@@ -9,15 +9,12 @@ class Alert
 	public const RED = "alert-danger";
 	public const GREEN = "alert-success";
 
-	public static function addAlert(string $type, string $text)
+	public static function addAlert(string $type, string $message)
 	{
-		Session::put('alert',[
-			"type" => $type,
-			"text" => $text
-		]);
+		Session::put('alert', ["type" => $type, "text" => $message]);
 		// $_SESSION['alert'][] = [
 		// 	"type" => $type,
-		// 	"text" => $text
+		// 	"text" => $message
 		// ];
 	}
 }
--- Utils/Session.php
+++ Utils/Session.php
@@ -6,7 +6,7 @@ class Session
 {
 
     public static function put($key, $value){
-        $_SESSION[$key] = $value;
+        $_SESSION[$key][] = $value;
     }
 
     public static function get($key){
