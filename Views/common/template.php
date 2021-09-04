2238ea8b119d706219a02969c1c2788a0b95bf32
L-TCD <loic.coissard@gmail.com>
2021-09-04 13:31:26 +0200

    add css theme bootswatch

--- Views/common/template.php
+++ Views/common/template.php
@@ -5,8 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= filter_var($pageDescription, FILTER_SANITIZE_STRING); ?>">
+	<meta name="robots" content="noindex">
     <title><?= filter_var($pageTitle, FILTER_SANITIZE_STRING); ?></title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+	<!-- <link rel="stylesheet" href="https://bootswatch.com/5/sandstone/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="./src/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </head>
 <body>
