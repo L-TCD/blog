22ef948f33211b4d8b7befe099840cdce99ce654
L-TCD <loic.coissard@gmail.com>
2021-09-04 11:48:03 +0200

    post.view improvement

--- Views/post.view.php
+++ Views/post.view.php
@@ -1,11 +1,11 @@
-<h1 class="text-center mt-3"><?= htmlentities($post->getTitle()) ?></h1>
+<h1 class="text-center mt-3"><?= filter_var($post->getTitle(), FILTER_SANITIZE_STRING) ?></h1>
 <hr>
-<p class="text-muted">Publié le <?= htmlentities($post->getCreatedAt()->format('d/m/Y')) ?> par <?= htmlentities($post->getAuthor()) ?>.
+<p class="text-muted">Publié le <?= filter_var($post->getCreatedAt()->format('d/m/Y'), FILTER_SANITIZE_STRING) ?> par <?= filter_var($post->getAuthor()) ?>.
 <?php if(!empty($post->getUpdateAt())) : ?>
-	(Dernière modification le <?= htmlentities($post->getUpdateAt()->format('d/m/Y à H:i:s')) ?>)</p>
+	(Dernière modification le <?= filter_var($post->getUpdateAt()->format('d/m/Y à H:i:s'), FILTER_SANITIZE_STRING) ?>)</p>
 <?php endif ?>
-<p class="text-muted"><?= nl2br(htmlentities($post->getDescription())) ?></p><br>
-<p><?= nl2br(htmlentities($post->getContent())) ?></p>
+<p class="text-muted"><?= nl2br(filter_var($post->getDescription(), FILTER_SANITIZE_STRING)) ?></p><br>
+<p><?= nl2br(filter_var($post->getContent(), FILTER_SANITIZE_STRING)) ?></p>
 <br>
 
 <h2 class="text-center">Commentaires</h2>
@@ -15,28 +15,28 @@
 	<?php if($admin) : ?>
 		<div class="mb-5">
 			<div class="text-muted mb-2">
-				Le <?= $comment->getCreatedAt()->format('d/m/Y à H:i:s') ?> par <?= htmlentities($comment->getUsername()) ?>
+				Le <?= filter_var($comment->getCreatedAt()->format('d/m/Y à H:i:s'), FILTER_SANITIZE_STRING) ?> par <?= filter_var($comment->getUsername(), FILTER_SANITIZE_STRING) ?>
 				<form action="/comment/show" method="POST" style="display:inline">
-					<input type="hidden" name="post_id" value="<?= htmlentities($post->getId()) ?>">
-					<input type="hidden" name="id" value="<?= htmlentities($comment->getId()) ?>">
+					<input type="hidden" name="post_id" value="<?= filter_var($post->getId(), FILTER_VALIDATE_INT) ?>">
+					<input type="hidden" name="id" value="<?= filter_var($comment->getId(), FILTER_VALIDATE_INT) ?>">
 					<button class="btn btn-primary ms-1 <?= ($comment->getValid())?"disabled":"" ?>" type="submit">Afficher</button>
 				</form>
 
 				<form action="/comment/hide" method="POST" style="display:inline">
-					<input type="hidden" name="post_id" value="<?= htmlentities($post->getId()) ?>">
-					<input type="hidden" name="id" value="<?= htmlentities($comment->getId()) ?>">
+					<input type="hidden" name="post_id" value="<?= filter_var($post->getId(), FILTER_VALIDATE_INT) ?>">
+					<input type="hidden" name="id" value="<?= filter_var($comment->getId(), FILTER_VALIDATE_INT) ?>">
 					<button class="btn btn-secondary ms-1 <?= ($comment->getValid())?"":"disabled" ?>" type="submit">Masquer</button>
 				</form>
 
 				<form action="/comment/updateForm" method="POST" style="display:inline">
-					<input type="hidden" name="post_id" value="<?= htmlentities($post->getId()) ?>">
-					<input type="hidden" name="id" value="<?= htmlentities($comment->getId()) ?>">
+					<input type="hidden" name="post_id" value="<?= filter_var($post->getId(), FILTER_VALIDATE_INT) ?>">
+					<input type="hidden" name="id" value="<?= filter_var($comment->getId(), FILTER_VALIDATE_INT) ?>">
 					<button class="btn btn-warning ms-1" type="submit">Modifier</button>
 				</form>
 
 				<form action="/comment/delete" method="POST" onSubmit="return confirm('Voulez-vous vraiment supprimer le commentaire ?');" style="display:inline">
-					<input type="hidden" name="post_id" value="<?= htmlentities($post->getId()) ?>">
-					<input type="hidden" name="id" value="<?= htmlentities($comment->getId()) ?>">
+					<input type="hidden" name="post_id" value="<?= filter_var($post->getId(), FILTER_VALIDATE_INT) ?>">
+					<input type="hidden" name="id" value="<?= filter_var($comment->getId(), FILTER_VALIDATE_INT) ?>">
 					<button class="btn btn-danger ms-1">Supprimer</button>
 				</form>
 
@@ -53,14 +53,14 @@
 					<form action="/comment/update" method="POST">
 						<div class="form-group">
 							<label for="content" class="form-label">Message à modifier:</label>
-							<textarea name="content" id="content" class="form-control" cols="auto" rows="auto"><?= nl2br(htmlentities($comment->getContent())) ?></textarea>
+							<textarea name="content" id="content" class="form-control" cols="auto" rows="auto"><?= nl2br(filter_var($comment->getContent(), FILTER_SANITIZE_STRING)) ?></textarea>
 						</div>
-						<input type="hidden" name="id" value="<?= htmlentities($comment->getId()) ?>">
-						<input type="hidden" name="post_id" value="<?= htmlentities($post->getId()) ?>">
+						<input type="hidden" name="id" value="<?= filter_var($comment->getId(), FILTER_VALIDATE_INT) ?>">
+						<input type="hidden" name="post_id" value="<?= filter_var($post->getId(), FILTER_VALIDATE_INT) ?>">
 						<button type="submit" class="btn btn-warning my-3">Enregistrer</button>
 					</form>
 				<?php else : ?>
-				<?= nl2br(htmlentities($comment->getContent())) ?></p>
+				<?= nl2br(filter_var($comment->getContent(), FILTER_SANITIZE_STRING)) ?></p>
 				<?php endif ?>
 			</div>
 		</div>
@@ -68,10 +68,10 @@
 		<?php if($comment->getValid() === true) : ?>
 			<div class="mb-5">
 				<div class="text-muted mb-2">
-					Le <?= htmlentities($comment->getCreatedAt()->format('d/m/Y à H:i:s')) ?> par <?= htmlentities($comment->getUsername()) ?> :
+					Le <?= filter_var($comment->getCreatedAt()->format('d/m/Y à H:i:s'), FILTER_SANITIZE_STRING) ?> par <?= filter_var($comment->getUsername(), FILTER_SANITIZE_STRING) ?> :
 				</div>
 				<div class="mx-3">
-					<?= nl2br(htmlentities($comment->getContent())) ?></p>
+					<?= nl2br(filter_var($comment->getContent(), FILTER_SANITIZE_STRING)) ?></p>
 				</div>
 			</div>
 		<?php endif ?>
@@ -87,7 +87,7 @@
 			<div class="form-group">
 				<textarea name="content" id="content" class="form-control" cols="auto" rows="2"></textarea>
 			</div>
-			<input type="hidden" name="post_id" value="<?= htmlentities($post->getId()) ?>">
+			<input type="hidden" name="post_id" value="<?= filter_var($post->getId(), FILTER_VALIDATE_INT) ?>">
 			<button type="submit" class="btn btn-success my-3">Envoyer</button>
 		</form>
 	</div>
